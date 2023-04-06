68fa0e0a94047c217ffff100027cbb94a125e01e
TatianaNahanova <127849913+TatianaNahanova@users.noreply.github.com>
2023-04-06 17:54:20 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,9 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 230790372 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    time = 85
+    acceleration = 2 * x / time**2
+    return acceleration.mean()
